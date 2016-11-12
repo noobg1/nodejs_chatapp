460857b3ccf7599cf0199669a673c47bff3436f7
noobg1 <jeevu.art@gmail.com>
2016-11-12 08:07:00 +0530

    basic emojis

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
+	request = require("request"),
 	nicknames = [];
 
 server.listen(3000);
@@ -24,7 +25,15 @@ io.sockets.on('connection',function(socket){
 	});
 
 	socket.on('send message',function(data){
-		io.sockets.emit('new message',{msg : data, nick : socket.nickname});
+		var url = "http://127.0.0.1:5000/q/"+data, senti, sentiParsed
+
+		request(url, function(error, response, body) {
+			 senti = JSON.parse(body)
+			 sentiParsed = String(senti['result'])
+		  console.log(sentiParsed);
+		  io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
+		});
+		
 
 	});
 
